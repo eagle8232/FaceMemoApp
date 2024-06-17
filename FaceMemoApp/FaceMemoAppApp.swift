8e2d9d34ab9b55c2ecc91f166c91153b77d0454f
Vusal <zevs.1418@gmail.com>
2024-06-17 18:47:20 +0400

    Initial Commit

---
+++ FaceMemoApp/FaceMemoAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  FaceMemoAppApp.swift
+//  FaceMemoApp
+//
+//  Created by Vusal Nuriyev 2 on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct FaceMemoAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
