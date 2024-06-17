634d8a27fd1056224d936523fd34aeb4aa059495
Vusal <zevs.1418@gmail.com>
2024-06-17 19:08:55 +0400

    Project is Cleaned.
    Project is cleaned and some files & folders are implemented.

---
+++ FaceMemoApp/UI/UI Helpers/TabBar/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  FaceMemoApp
+//
+//  Created by Vusal Nuriyev 2 on 17.06.24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
---
+++ FaceMemoApp/UI/UI Helpers/TabBar/Tabs.swift
@@ -0,0 +1,12 @@
+//
+//  Tabs.swift
+//  FaceMemoApp
+//
+//  Created by Vusal Nuriyev 2 on 17.06.24.
+//
+
+import SwiftUI
+
+enum Tabs: CaseIterable {
+    case home
+}
